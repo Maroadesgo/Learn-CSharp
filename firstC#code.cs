3cbdc6b9193831da0638cc730e10c9bd25e9801c
rony espinosa <129472123+Maroadesgo@users.noreply.github.com>
2026-02-02 21:36:14 -0400

    Add initial C# code for greeting and temperature

---
+++ firstC#code.cs
@@ -0,0 +1,5 @@
+string firstName = "Bob";
+int numberOfMessage = 3;
+decimal temperature = 34.4m;
+
+Console.WriteLine("Hello " + firstName + " ! " + "you have "+ numberOfMessage + " in you inbox. "+ "The temperature is " + temperature + " celcius.");
